31ca7436431e4a5713bc03f530c78acdb8226a51
Gabriel Victor Raymundo <bieelvii13@gmail.com>
2023-01-07 23:42:05 -0300

    Corrigido o toggle do menu

--- js/toggleMenuMobile.js
+++ js/toggleMenuMobile.js
@@ -2,16 +2,11 @@ const linksMenu = document.getElementById('links');
 const menuHamburguer = document.getElementById('menu-hamburguer');
 const telaInteira = document;
 
-menuHamburguer.addEventListener("click", () => {
-    toggle(linksMenu, 'displayFlex');
-});
-
-telaInteira.addEventListener("click", (e) => { 
+telaInteira.addEventListener("click", (e) => {
     if (!linksMenu.contains(e.target)) {
         if (menuHamburguer.contains(e.target)) {
-            toggle(linksMenu, 'displayNone');
+            toggle(linksMenu, 'displayFlex');
         } else {
-            linksMenu.classList.add('displayNone');
             linksMenu.classList.remove('displayFlex');
         }                
     }       
